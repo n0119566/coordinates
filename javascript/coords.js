099a92a9abc20765204ac188cea58d6b70936e69
Christopher-Adzima_lmig <Christopher.Adzima@LibertyMutual.com>
2023-04-17 09:53:13 -0400

    Minor formatting change

--- javascript/coords.js
+++ javascript/coords.js
@@ -1,13 +1,15 @@
 const coordsButton = document.querySelector("#coords");
-const coordsDisplay = document.querySelector("#display");
+const latDisplay = document.querySelector("#lat");
+const longDisplay = document.querySelector("#long");
 
-coordsButton.addEventListener("click", function(){
-
+coordsButton.addEventListener("click", function () {
   // Get the postion from the browser
   navigator.geolocation.getCurrentPosition(function (position) {
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
-    coordsDisplay.textContent = `Latitude: ${latitude} Longitude: ${longitude}`;
 
+    // Display them in the browser
+    latDisplay.textContent = `Latitude: ${latitude} `;
+    longDisplay.textContent = `Longitude: ${longitude}`;
   });
 });
