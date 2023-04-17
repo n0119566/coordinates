17989176a1b9a376fc2f3ac77393791b29d3dfea
Christopher-Adzima_lmig <Christopher.Adzima@LibertyMutual.com>
2023-04-17 13:30:57 -0400

    Added a copy button to make easier to log

--- javascript/coords.js
+++ javascript/coords.js
@@ -1,6 +1,7 @@
 const coordsButton = document.querySelector("#coords");
 const latDisplay = document.querySelector("#lat");
 const longDisplay = document.querySelector("#long");
+const copyButton = document.querySelector("#copy");
 
 coordsButton.addEventListener("click", function () {
   // Get the postion from the browser
@@ -11,5 +12,20 @@ coordsButton.addEventListener("click", function () {
     // Display them in the browser
     latDisplay.textContent = `Latitude: ${latitude} `;
     longDisplay.textContent = `Longitude: ${longitude}`;
+
+    // Unhide the copy button
+    copyButton.classList.remove("d-none");
   });
+});
+
+// Copy long and lat to the clipboard to make life easy
+copyButton.addEventListener("click", () => {
+  const lat = latDisplay.textContent;
+  const long = longDisplay.textContent;
+
+  // Make sure we have long and length and copy
+  if (lat.length > 0 && long.length > 0) {
+    const copyText = lat + " " + long;
+    navigator.clipboard.writeText(copyText);
+  }
 });
